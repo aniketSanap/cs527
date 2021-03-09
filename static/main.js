6b635740cd72841a0b5df49aa99a9d4052cabd84
Aniket Sanap <aniket.s2509@gmail.com>
2021-03-08 22:16:07 -0500

    Fixed css and merged history and gui tabs

--- static/main.js
+++ static/main.js
@@ -1,40 +1,53 @@
 $(document).ready(function() {
+    $('#history-tab').click(function() {
+        get_saved_data();
+    });
+
     $('#submit-button').click(function() {
         $('#submit-button').css('display', 'none');
         $('#loading').css('display', 'block');
         query = getSelectedTextById("textbox");
         database_type = $('input[name=database_type]:checked').val()
-        $.ajax({
-            type: "POST",
-            dataType: "json",
-            url: "/post",
-            data: {
-                query_string: query, 
-                database_type: database_type
-            },
-            success: function(data) {
-                console.log(data)
-                $('#submit-button').css('display', 'block');
-                $('#loading').css('display', 'none');
-                if (data['success']) {
-                    rows = JSON.parse(data['rows']);
-                    rowCount = data['row_count'];
-                    if (rowCount == '0' || rows.length == 0) {
-                        refreshTable('display-table', 'display-table');
-                        refreshTable('summary-table', 'summary-table');
-                    } else {
-                        loadTable(rows, data['delimiter'], 'display-table', 'display-table');
-                        loadTable(JSON.parse(data['summary']), data['delimiter'], 'summary-table', 'summary-table');
-                    }
-                } else {
+        get_output(query, database_type);
+    })
+
+    $('#history-tab').click();
+});
+
+function get_output(query, database_type) {
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "/post",
+        data: {
+            query_string: query, 
+            database_type: database_type
+        },
+        success: function(data) {
+            console.log(data);
+            $('#submit-button').css('display', 'block');
+            $('#loading').css('display', 'none');
+            if (data['success']) {
+                rows = JSON.parse(data['rows']);
+                rowCount = data['row_count'];
+                if (rowCount == '0' || rows.length == 0) {
                     refreshTable('display-table', 'display-table');
                     refreshTable('summary-table', 'summary-table');
+                } else {
+                    $('#output-tab').click();
+                    $('#output-tab').css('display', 'block');
+                    $('#summary-tab').css('display', 'block');
+                    loadTable(rows, data['delimiter'], 'display-table', 'display-table');
+                    loadTable(JSON.parse(data['summary']), data['delimiter'], 'summary-table', 'summary-table');
                 }
-                displayMessage(data['success'], data['row_count'], data['run_time']);
+            } else {
+                refreshTable('display-table', 'display-table');
+                refreshTable('summary-table', 'summary-table');
             }
-        });
-    })
-});
+            displayMessage(data['success'], data['row_count'], data['run_time']);
+        }
+    });
+}
 
 function loadTable(data, delim, id, class_) {
     console.log(data)
@@ -52,7 +65,6 @@ function loadTable(data, delim, id, class_) {
         data : data,
         // "bPaginate": false,
         bFilter: true,
-        bSort: true,
         bInfo: true,
         columns: column_object,
     }
@@ -68,18 +80,18 @@ function displayMessage(code, num_rows, run_time) {
     $("#success-message").css("display", "none");
     $("#error-message").css("display", "block");
   }
-
-function get_saved_data(){
-  $.ajax({
-    type: "POST",
-    url: "/get_query_history",    
-    success: function (data) {
-      console.log(data)
-    },
-  });
 }
 
-}
+function get_saved_data() {
+    $.ajax({
+      type: "POST",
+      url: "/get_query_history",    
+      success: function (data) {
+        console.log(data);
+        loadTable(JSON.parse(data), '$|$|', 'history-table', 'history-table');
+      },
+    });
+  }
 
 
 function refreshTable(class_, id) {
